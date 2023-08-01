92ef50f35f3c03e23d8da78751fac16daef3bae4
priyanga_pas <apriyangasingh@gmail.com>
2023-08-01 14:54:50 -0700

    day1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+builders = [
+    {"region": "Northern",
+    "house_type": "Ranch",
+    "rating": "5",
+    "budget_category": "low",
+    "name": "kb house",
+    "appliances_included": False
+    },
+        {"region": "western",
+    "house_type": "single family",
+    "rating": "5",
+    "budget_category": "high",
+    "name": "cresleigh",
+    "appliances_included": True
+    },
+        {"region": "southern",
+    "house_type": "condo",
+    "rating": "4",
+    "budget_category": "high",
+    "name": "premier",
+    "appliances_included": False
+    },
+]
 # Create your views here.
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'about.html')
+
+def builders_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'builders/index.html', {
+    'builders': builders
+  })
